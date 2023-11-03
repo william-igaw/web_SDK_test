2b59b2215978f42cfd34593717f7ef124209fcfc
william-igaw <49185783+william-igaw@users.noreply.github.com>
2023-11-03 09:15:01 +0900

    Delete DefaultSDKTestWeb directory

--- DefaultSDKTestWeb/init.sh
+++
@@ -1,2 +0,0 @@
-sed -i -e "s;%DFN_APP_KEY%;$DFN_APP_KEY;g" -e "s;%DFN_SECRET_KEY%;$DFN_SECRET_KEY;g" ./js/main.js
-busybox httpd -f -v -p 80
--- DefaultSDKTestWeb/service-worker.js
+++
@@ -1,1 +0,0 @@
-importScripts('https://s3.ap-northeast-1.amazonaws.com/static.adbrix.io/web-sdk/test/service-worker.js');
